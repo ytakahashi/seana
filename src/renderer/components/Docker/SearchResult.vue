ee1fbb4ea95e57e146e86bd61e56db814e2a1832
ytakahashi <yt01tc@gmail.com>
2018-10-08 11:51:44 +0900

    loading during pulling image from docker hub

--- src/renderer/components/Docker/SearchResult.vue
+++ src/renderer/components/Docker/SearchResult.vue
@@ -33,23 +33,34 @@
       </a>
       <a class="button is-link is-outlined" @click="pull">
         Pull This Image
-        <b-loading :is-full-page="true" :active.sync="isLoading" :can-cancel="false"></b-loading>
       </a>
     </div>
 
     <div class="content">
       <div class="field has-addons" v-if="command">
 
-        <article class="message is-warning">
+        <div class="pull-succeed-msg-box">
+            <p>following command has been run:</p>
+            <code>
+            $ {{ command }}
+            </code>
+        </div>
+
+        <!-- <article class="message is-warning">
           <div class="message-body">
             following command has been run: <br />
             <code>
             $ {{ command }}
             </code>
           </div>
-        </article>
+        </article> -->
 
       </div>
+    </div>
+
+    <div class="pull-err-msg-box" v-if="pullError">
+      <span class="box-title">Failed to pull image.</span>
+      <pre>{{ pullError }}</pre>
     </div>
 
   </div>
@@ -58,12 +69,13 @@
 
 <script>
   const { shell } = require('electron')
-  const execSync = require('child_process').execSync
+  const { exec } = require('child_process')
 
   export default {
     data () {
       return {
         command: null,
+        pullError: null,
         isLoading: false
       }
     },
@@ -90,6 +102,19 @@
       }
     },
     methods: {
+      dockerPull (name, loading) {
+        const cmd = `docker pull ${name}`
+        exec(cmd, (error, stdout, stderr) => {
+          if (error) {
+            this.pullError = String(error)
+            loading.close()
+            console.log(this.pullError)
+            return
+          }
+          this.command = cmd
+          loading.close()
+        })
+      },
       view () {
         const baseUrl = 'https://hub.docker.com/'
         const path = this.official
@@ -99,12 +124,42 @@
         shell.openExternal(`${baseUrl}${path}/${this.name}`)
       },
       pull () {
-        this.isLoading = true
-        this.command = `docker pull ${this.name}`
-        const result = execSync(this.command).toString()
-        console.log(result)
-        this.isLoading = false
+        const loading = this.$loading.open()
+        this.dockerPull(this.name, loading)
       }
     }
   }
 </script>
+
+<style scoped>
+  .pull-succeed-msg-box {
+      padding: 2em 1em;
+      margin: 0em;
+      color: #474747;
+      background: rgb(246, 253, 252);
+      border-left: double 7px #4ec4d3;
+      border-right: double 7px #4ec4d3;
+  }
+
+  .pull-err-msg-box {
+      position: relative;
+      padding: 30px 10px 10px;
+      border: solid 2px #ff7f50;
+      border-radius: 10px 10px 10px 10px;
+  }
+
+  .pull-err-msg-box .box-title {
+      position: absolute;
+      display: inline-block;
+      top: -2px;
+      left: -2px;
+      padding: 0 9px;
+      height: 25px;
+      line-height: 25px;
+      vertical-align: middle;
+      font-size: 17px;
+      background: #ff7f50;
+      color: #ffffff;
+      font-weight: bold;
+  }
+</style>
